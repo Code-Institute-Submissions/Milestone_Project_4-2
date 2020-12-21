03bdc722712e02175a98901cc9eb0a43f2a023c4
MikeVez <mikevez14@gmail.com>
2020-12-21 18:41:41 +0000

    profile models.py created

--- profiles/models.py
+++ profiles/models.py
@@ -1,2 +1,20 @@
 from django.db import models
+from django_countries.fields import CountryField
 from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    zip_or_post = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=True, blank=True)
+    points = models.IntegerField(null=True, blank=True, default=0)
+    total_savings = models.IntegerField(null=True, blank=True, default=0)
+
+    def __str__(self):
+        return self.user.username
