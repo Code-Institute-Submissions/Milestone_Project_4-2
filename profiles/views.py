f62ff0f1620843f24f1d5e4863dd9bcaa07d39eb
MikeVez <mikevez14@gmail.com>
2020-12-21 19:08:48 +0000

    views.py profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import UserProfileForm
+from .models import UserProfile
+from payment.models import Order
+
+@login_required
+def profile(request):
+    """ Display customers profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully.')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        'test1'
+    ))
+
+    template = 'payment/payment_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
