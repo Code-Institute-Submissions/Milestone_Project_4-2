2a95e6c83e4b0376b41f163930ae9aeefa1638b6
MikeVez <mikevez14@gmail.com>
2021-01-02 14:09:47 +0000

    urls.py directs added

--- Milestone4/urls.py
+++ Milestone4/urls.py
@@ -4,6 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('cart/', include('cart.urls')),
+    path('profile/', include('profiles.urls')),
+    path('payment/', include('payment.urls')),
     path('posters/', include('posters.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
