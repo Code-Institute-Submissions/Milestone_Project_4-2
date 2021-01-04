9f7a25f8bf4c92282d2943eedc617d2830641e15
MikeVez <mikevez14@gmail.com>
2021-01-04 12:47:23 +0000

    secret key

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+'''SECRET_KEY = os.getenv('SECRET_KEY')'''
+SECRET_KEY = '^6pik&7y@z+9nbi*98wig3or4#%3hv4ldw%jq=v!f9q1yqyagy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
