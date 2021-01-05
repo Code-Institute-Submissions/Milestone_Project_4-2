05633b66667e31ac809fabc033848eeb2c932884
MikeVez <mikevez14@gmail.com>
2021-01-05 11:44:53 +0000

    commit reverted to old, payments files updated

---
+++ payment/views.py
@@ -0,0 +1,165 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.conf import settings
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from posters.models import Poster
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+from cart.contexts import cart_contents
+
+import stripe
+import json
+
+def payment(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Keep shopping. Your cart is empty.")
+        return redirect(reverse('posters'))
+
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'zip_or_post': request.POST['zip_or_post'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            #pid = request.POST.get('client_secret').split('_secret')[0]
+            #order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
+            for item_id, item_data in cart.items():
+                try:
+                    poster = Poster.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        poster=poster,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
+                except Poster.DoesNotExist:
+                    messages.error(request, (
+                        "One of the posters in your cart was not found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('payment_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "Keep shopping. Your cart is empty.")
+            return redirect(reverse('posters'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+    
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'zip_or_post': profile.default_zip_or_post,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+                order_form = OrderForm()
+
+    template = 'payment/payment.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+def payment_success(request, order_number):
+    """
+    Handle successful payments
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        current_cart = cart_contents(request)
+        order.user_profile = profile
+        
+        points_earned = order.order_total * 100
+        order.points_earned = points_earned
+        profile_points = profile.points
+        profile.points = profile_points + points_earned
+        
+        total_savings = current_cart['savings_total']
+        order.savings_total = current_cart['savings_total']
+        profile_savings = profile.total_savings
+        profile.total_savings += total_savings
+        
+        order.save()
+        profile.save()
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_zip_or_post': order.zip_or_post,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+
+
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'payment/payment_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
