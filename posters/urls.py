2c733b1b6ed772bedb13b6e6c945819b218f84ab
MikeVez <mikevez14@gmail.com>
2021-01-11 09:01:49 +0000

    poster urls modified

--- posters/urls.py
+++ posters/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_posters, name='posters'),
-    path('<int:poster_id>/', views.poster_info, name='poster_info'),
+    path('<int:poster_id>/', views.poster_detail, name='poster_detail'),
     path('add/', views.add_poster, name='add_poster'),
     path('edit/<int:poster_id>/', views.edit_poster, name='edit_poster'),
-    path('delete/<int:poster_id>/', views.delete_poster, name='delete_poster')
+    path('delete/<int:poster_id>/', views.delete_poster, name='delete_poster'),
 ]
