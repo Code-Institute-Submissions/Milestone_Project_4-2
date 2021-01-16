8ae904b55f1673ed9a543d6fc68023b6e4a86c73
MikeVez <mikevez14@gmail.com>
2021-01-16 11:34:29 +0000

    poster category links

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-from .forms import UserProfileForm
 from .models import UserProfile
 from payment.models import Order
+from .forms import UserProfileForm
 
 @login_required
 def profile(request):
-    """ Display customers profile. """
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -34,7 +34,8 @@ def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        'test1'
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
     ))
 
     template = 'payment/payment_success.html'
