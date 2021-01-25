3725ed9c42137c22d3ebfea94a04fcb26516eb5c
MikeVez <mikevez14@gmail.com>
2021-01-25 11:44:51 +0000

    migrations redone

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -97,8 +97,7 @@ WSGI_APPLICATION = 'Milestone4.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-
-    DATABASES = { 
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
         }
 else:
